7f5bb1eeaf9c6a7e551dd52a595d325c3bad6faf
ahmadhilmandani <ahmadhilmanlagi@gmail.com>
2022-09-30 20:53:04 +0700

    Coba1

--- application/controllers/C_dashboard.php
+++ application/controllers/C_dashboard.php
@@ -20,6 +20,7 @@ class C_dashboard extends CI_Controller
 		$data["vaksin_keberapa"] = $vaksin_keberapa;
 		$data["data_click_sidebar"] = $vaksin_keberapa;
 
+
 		if($vaksin_keberapa == "Pertama"){
 			$data["nama_vaksin"] = "sinovac";
 		}
@@ -31,6 +32,9 @@ class C_dashboard extends CI_Controller
 		}
 		else if($vaksin_keberapa == "Keempat"){
 			$data["nama_vaksin"] = "sinovac-4";
+		}
+		else{
+			$this->model->load();
 		}
 
 		$data['sidebar_user'] = $this->load->view('templates/user/View_sidebar_user', NULL, TRUE);
