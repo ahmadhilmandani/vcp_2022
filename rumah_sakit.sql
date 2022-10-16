5e2750d483e8407b7eae29d1654d4d9e62acf75a
Triyono Rifan <triyonorifan01@gmail.com>
2022-10-16 07:26:53 +0700

    update table database

--- rumah_sakit.sql
+++ rumah_sakit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Okt 2022 pada 16.03
+-- Waktu pembuatan: 16 Okt 2022 pada 02.26
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 7.4.27
 
@@ -45,7 +45,9 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`nik_id_admin`, `sandi`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `perkerjaan`, `alamat`, `is_admin`) VALUES
 ('3214223312340002', '123', 'Ahmad Hilman Dani', '2002-05-27', 'pria', 'PNS', 'Sumenep', 0),
 ('3214223312340003', '123', 'Cagiva Chaedar BY', '2002-05-01', 'pria', 'PNS', 'Sumenep', 0),
-('3529121109010002', '123', 'Triyono Rifan', '2001-09-11', 'pria', 'Mahasiswa', 'Ambunten', 1);
+('3529121109010002', '123', 'Triyono Rifan', '2001-09-11', 'pria', 'Mahasiswa', 'Ambunten', 1),
+('3529121208020001', '123', 'Dian', '2012-10-26', 'wanita', 'Guru', 'Ambunten', 0),
+('3529161209020012', '123', 'mardi', '2012-10-26', 'wanita', 'Guru', 'Ambunten', 0);
 
 -- --------------------------------------------------------
 
@@ -59,16 +61,18 @@ CREATE TABLE `vaksin` (
   `dosis` varchar(30) NOT NULL,
   `nama_vaksin` varchar(30) NOT NULL,
   `nama_user` varchar(150) NOT NULL,
-  `tanggal_vaksin` date NOT NULL
+  `tanggal_vaksin` date NOT NULL,
+  `perkerjaan` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `vaksin`
 --
 
-INSERT INTO `vaksin` (`id_vaksin`, `nik_user`, `dosis`, `nama_vaksin`, `nama_user`, `tanggal_vaksin`) VALUES
-(1, '3214223312340002', 'pertama', 'Sinovac-2', 'Ahmad Hilman Dani', '2022-10-06'),
-(2, '3214223312340003', 'pertama', 'Sinovac-2', 'Cagiva Chaedar BY', '2022-10-06');
+INSERT INTO `vaksin` (`id_vaksin`, `nik_user`, `dosis`, `nama_vaksin`, `nama_user`, `tanggal_vaksin`, `perkerjaan`) VALUES
+(1, '3214223312340002', 'pertama', 'Sinovac-2', 'Ahmad Hilman Dani', '2022-10-06', 'PNS'),
+(2, '3214223312340003', 'pertama', 'Sinovac-2', 'Cagiva Chaedar BY', '2022-10-06', 'PNS'),
+(10, '3529121208020001', 'Pertama', 'Sinopharm', 'Dian', '2022-10-19', 'Guru');
 
 -- --------------------------------------------------------
 
@@ -94,8 +98,7 @@ CREATE TABLE `vaksin_on_progress` (
 --
 
 INSERT INTO `vaksin_on_progress` (`nik_user`, `nama_user`, `dosis`, `nama_vaksin`, `tempat_vaksin`, `tanggal_vaksin`, `tanggal_vaksin_mulai`, `nomor_antrian`, `tanggal_vaksin_akhir`, `kuota`) VALUES
-('-', '-', 'Kedua', 'Sinopharm', 'puskesmas pamolokan', '0000-00-00', '2022-10-15', 1, '2022-10-29', 23),
-('3214223312340002', 'Ahmad Hilman Dani', 'Kedua', 'Sinopharm', 'puskesmas pamolokan', '2022-10-17', '0000-00-00', 2, '0000-00-00', 0);
+('-', '-', 'Pertama', 'Sinopharm', 'puskesmas pamolokan', '0000-00-00', '2022-10-15', 1, '2022-10-29', 23);
 
 --
 -- Indexes for dumped tables
@@ -128,13 +131,13 @@ ALTER TABLE `vaksin_on_progress`
 -- AUTO_INCREMENT untuk tabel `vaksin`
 --
 ALTER TABLE `vaksin`
-  MODIFY `id_vaksin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_vaksin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `vaksin_on_progress`
 --
 ALTER TABLE `vaksin_on_progress`
-  MODIFY `nomor_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `nomor_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
