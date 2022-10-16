0c84ed743142361e2510c8a0900378345e534ac6
ahmadhilmandani <ahmadhilmanlagi@gmail.com>
2022-10-17 06:39:47 +0700

    DEL View_login and View_sweet_alert

--- application/views/templates/View_login.php
+++
@@ -1,5 +0,0 @@
-<form action="" method="POST">
-    <input type="text" name="nik" id="nik" placeholder="NIK">
-    <input type="password" name="sandi" id="sandi" placeholder="Sandi">
-    <button type="submit" name="login">Login</button>
-</form>
