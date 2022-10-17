fbfbfe0e7c2678b5f0e3a3ef58f82d85416d2abf
ahmadhilmandani <ahmadhilmanlagi@gmail.com>
2022-10-17 14:10:39 +0700

    UPDATE view_login

--- application/views/combine/View_login.php
+++ application/views/combine/View_login.php
@@ -1,30 +1,46 @@
 <main class="container-fluid" style="height: 100vh;">
     <div class="row" style="height: 100vh;">
-        <div class="col-7 d-flex justify-content-center align-items-center flex-column position-relative">
+        <div class="col-lg-7 text-md-center text-lg-start d-flex justify-content-center align-items-center flex-column position-relative" style="height: 100vh;">
             <div>
-                <div class="mb-5">
-                    <h1>Pulih lebih cepat <br>bangkit lebih kuat!</h1>
+                <div>
+                    <h1 class="h1">Pulih lebih cepat <br>bangkit lebih kuat!</h1>
                 </div>
+
                 <form action="" method="POST">
-                    <div class="my-4">
-                        <input type="text" name="nik" id="nik" placeholder="NIK" class="px-2 py-1 w-75">
+                    <div class="row">
+                        <div class="mt-3 col-lg-9">
+                            <input type="text" name="nik" id="nik" placeholder="NIK" class="form-control ">
+                        </div>
                     </div>
-                    <div class="my-4">
-                        <input type="password" name="sandi" id="sandi" placeholder="Sandi" class="px-2 py-1 w-75">
+
+                    <div class="row">
+                        <div class="mt-3 col-lg-9">
+                            <input type="password" name="sandi" id="sandi" placeholder="Sandi" class="form-control ">
+                        </div>
                     </div>
-                    <button type="submit" name="login" class="btn btn-info px-2 py-1 w-75">Login</button>
+
+                    <div class="row">
+                        <div class="mt-3 col-lg-9">
+                            <button type="submit" name="login" class="btn btn-info px-2 py-1 w-100">Login</button>
+                        </div>
+                    </div>
                 </form>
-                <p class="my-4 w-75 text-center">belum punya akun? <a href="<?= base_url('C_register') ?>" class="text-dark">daftar disini!</a></p>
+                <div class="row">
+                    <div class="mt-3 col-lg-9">
+                        <p class="text-center">belum punya akun? <a href="<?= base_url('C_register') ?>" class="text-dark">daftar disini!</a></p>
+                    </div>
+                </div>
             </div>
-            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute" style="width: 20px; top:100px; left:150px;">
-            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute" style="width: 10px; top:240px; right:5px;">
-            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute" style="width: 20px; top:260px; right:30px;">
-            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute" style="width: 30px; top:300px; right:5px;">
+
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 20px; top:100px; left:150px;">
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 10px; top:240px; right:5px;">
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 20px; top:260px; right:30px;">
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 30px; top:300px; right:5px;">
         </div>
 
         <div class="col-5 p-0">
-            <div class="w-100 h-100 position-relative" style="background-image: url(<?= base_url('asset/img/gradient-bg-miring.png') ?>);">
-                <img src="<?= base_url('asset/img/orang-senyum.png') ?>" class="position-absolute bottom-0 end-0" style="width: 490px;">
+            <div class="w-100 h-100 position-relative d-none d-lg-block" style="background-image: url(<?= base_url('asset/img/gradient-bg-miring.png') ?>); background-repeat:no-repeat;">
+                <img src="<?= base_url('asset/img/orang-senyum.png') ?>" class="position-absolute d-none d-lg-block bottom-0 end-0" style="width: 490px;">
             </div>
         </div>
     </div>
