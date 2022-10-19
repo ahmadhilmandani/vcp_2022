a738663c5bd952b2803534def99ccde60199a3d2
ahmadhilmandani <ahmadhilmanlagi@gmail.com>
2022-10-19 16:28:00 +0700

    UPDATE View Login

--- application/views/combine/View_login.php
+++ application/views/combine/View_login.php
@@ -1,15 +1,18 @@
 <main class="container-fluid" style="height: 100vh;">
     <div class="row" style="height: 100vh;">
-        <div class="col-lg-7 d-flex justify-content-center align-items-center">
+        <div class="col-lg-7 d-flex justify-content-center align-items-center position-relative" style="height: 100%;">
             <div class="container-lg row">
-                <div class="col-md-8 m-auto">
-                    <div class="">
-                        <img src="<?= base_url("asset/img/logo_bg_hilang.png") ?>" alt="logo ayok vaksin" style="width: 310px;" class="mb-2">
+                <div class="col-md-8 m-auto ">
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <img src="<?= base_url("asset/img/logo_bg_hilang.png") ?>" alt="logo ayok vaksin" style="width: 100%;" class="mb-2">
+                            <p class="mt-5 mb-3 mx-auto pb-1 h6 text-muted text-center">Pulih lebih cepat, Bangkit lebih kuat!</p>
+                        </div>
                     </div>
 
-                    <form action="" method="POST">
+                    <form action="" method="POST" class="">
                         <div class="row">
-                            <div class="mt-3 col-lg-9">
+                            <div class="col-lg-9">
                                 <input type="text" name="nik" id="nik" placeholder="NIK" class="form-control ">
                             </div>
                         </div>
@@ -28,7 +31,7 @@
                     </form>
                     <div class="row">
                         <div class="mt-3 col-lg-9">
-                            <p class="text-center">belum punya akun? <a href="<?= base_url('C_register') ?>" class="text-dark">daftar disini!</a></p>
+                            <p class="text-center">belum punya akun? <a href="<?= base_url('C_register') ?>" class="text-dark text-decoration-none" style="width: fit-content;  border-width:0px; border-bottom-width: 2px; border-image: linear-gradient(to right bottom, #76ECF4, #3392F4); border-style:solid;  border-image-slice: 1;">daftar disini!</a></p>
                         </div>
                     </div>
                 </div>
@@ -36,10 +39,13 @@
             </div>
 
 
-            <!-- <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 20px; top:100px; left:150px;"> 
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 20px; top:100px; left:150px;">
             <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 10px; top:240px; right:5px;">
             <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 20px; top:260px; right:30px;">
-            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 30px; top:300px; right:5px;"> -->
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-none d-lg-block" style="width: 30px; top:300px; right:5px;">
+
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-block d-lg-none" style="width: 30px; bottom:80px; right: 250px;">
+            <img src="<?= base_url('asset/img/green-plus.png') ?>" class="position-absolute d-block d-lg-none" style="width: 20px; bottom:40px; right:100px;">
         </div>
 
         <div class="col-5 p-0">
